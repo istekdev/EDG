9a518a94a3383a611c63e25cfa5fe38b62957015
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-12-14 18:45:22 -0600

    Update EDG64.py

--- EDG-Hashing/EDG64.py
+++ EDG-Hashing/EDG64.py
@@ -1,10 +1,10 @@
 def edg64(input):
   tag = int.from_bytes("EDG64".encode("utf-8"), "big")
   if len(input) == 0:
-    payload = b"\x00" * 16
-  elif len(input) < 16:
-    payload = (input * ((16 // len(input)) + 1))[:16]
+    payload = b"\x00" * 8
+  elif len(input) < 8:
+    payload = (input * ((16 // len(input)) + 1))[:8]
   else:
-    payload = input[:16]
-  integer = (2**32) // int.from_bytes(input, "big") % 2**32
+    payload = input[:8]
+  integer = tag * int.from_bytes(input, "big")
   return integer.to_bytes(8, "big")
